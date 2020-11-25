4a099509ea0c7ab0f1d73fe60bfe8fd26c0d202f
Gastón Abellá <gaston@wyeworks.com>
2020-11-25 17:14:37 -0300

    Generalize endpoints to all sensors and playas

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -3,49 +3,86 @@ require 'rgeo/geo_json'
 class FeaturesController < ApplicationController
 
   def create
-    geom = geojson_decoder.decode(sensor_agua_create_params[:geom].to_h)
-    new_params = sensor_agua_create_params.to_h
+    current_params = feature_params(required: true)
+    geom = geojson_decoder.decode(current_params[:geom].to_h)
+    new_params = current_params.to_h
     new_params[:geom] = geom
 
-    sensor = SensorAgua.create(new_params)
+    feature = Feature.create_by_type(params[:feature_type], new_params)
 
-    render json: sensor.reload
+    render json: feature.reload
   end
 
   def update
-    sensor = SensorAgua.find(params[:id])
+    # Esto no anda todavia
+    # Habria que definir un método find_by_type en Feature
+    feature = Feature.find(params[:id])
 
-    new_params = sensor_agua_params.to_h
-    geom_param = sensor_agua_params[:geom]
+    current_params = feature_params(required: false)
+
+    new_params = current_params.to_h
+    geom_param = current_params[:geom]
     if geom_param.present?
       geom = geojson_decoder.decode(geom_param.to_h)
       new_params[:geom] = geom
     end
 
-    sensor.update(new_params)
+    feature.update_by_type(params[:feature_type], new_params)
 
-    render json: sensor
+    render json: feature
   end
 
   def destroy
-    sensor = SensorAgua.find(params[:id])
-    sensor.delete
+    # Esto tampoco todavia
+    feature = Feature.find(params[:id])
+    feature.delete
 
     head :no_content
   end
 
   private
 
-  def sensor_agua_params
-    params.require(:feature).permit([:id, :id_playa, :valor, geom: [:type, coordinates: []]])
+  def feature_params(required: false)
+    params.require(:feature_type)
+    feature_params_by_type(required)
   end
 
-  def sensor_agua_create_params
-    sensor_agua_params.tap do |sensor_params|
-      sensor_params.require([:id, :id_playa, :geom])
+  def feature_params_by_type(required)
+    case params[:feature_type]
+    when 'playa'
+      if required
+        playa_required_params
+      else
+        playa_params
+      end
+    else
+      if required
+        sensor_required_params
+      else
+        sensor_params
+      end
     end
   end
 
+  def playa_params
+    params.require(:feature).permit([:nombre, :puntaje, geom: [:type, coordinates: []]])
+  end
+
+  def playa_required_params
+    playa_params.tap do |p_params|
+      p_params.require([:nombre, :geom])
+    end
+  end
+
+  def sensor_params
+    params.require(:feature).permit([:id_playa, :valor, geom: [:type, coordinates: []]])
+  end
+
+  def sensor_required_params
+    sensor_params.tap do |s_params|
+      s_params.require([:id_playa, :geom])
+    end
+  end
 
   def geojson_decoder
     @geojson_decoder ||= RGeo::GeoJSON::Coder.new(geo_factory: RGeo::Geographic.spherical_factory(srid: 4326))
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,4 +1,31 @@
 
 class Feature < ApplicationRecord
   self.primary_key = 'gid'
+
+  class << self
+    def create_by_type(type, params)
+      feature_subclass(type).create(params)
+    end
+
+    def update_by_type(type, params)
+      feature_subclass(type).update(params)
+    end
+
+    private
+
+    def feature_subclass(type)
+      case type
+      when "sensor_uv"
+        SensorUv
+      when "sensor_banderas"
+        SensorBandera
+      when "sensor_personas"
+        SensorPersona
+      when "sensor_agua"
+        SensorAgua
+      when "playa"
+        Playa
+      end
+    end
+  end
 end
