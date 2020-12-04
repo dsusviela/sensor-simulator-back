d31f421906cdeb6a0629c6e2dd0ba86a82b668dd
Ana Machado <ana@wyeworks.com>
2020-12-03 22:13:57 -0300

    Add bus sensor simulator logic

--- app/models/bus_sensor.rb
+++ app/models/bus_sensor.rb
@@ -3,7 +3,23 @@ class BusSensor < ApplicationRecord
 
   after_create :provide_sensor_to_orion
 
+  def send_data
+    return unless alive
+    payload = {
+      value: next_location
+    }
+
+    OrionHelper.make_orion_post_request("#{ENV['IOT_AGENT_NORTH_URL']}/iot/json?k=#{ENV['ORION_API_KEY']}&i=Device#{id.to_s}", payload)
+  end
+
   private
+
+  def next_location
+    detour = Detour.where(line: line, subline: subline, direction: direction, location_index: location_index)&.first
+
+    # revisar si hay que convertir los datos para el payload porque no sé cómo se ve la data
+    detour&.location
+  end
 
   def provide_sensor_to_orion
     payload = {
