7b7df3b4446ce4adc7ab6974f5825e27f1bbd192
Ana Machado <ana@wyeworks.com>
2020-12-03 23:08:14 -0300

    192 detour geojson

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+GEOJSON_FILES_DIR = "../detours_geojsons".freeze
+
+geojson_files = Dir.entries(GEOJSON_FILES_DIR)
+
+geojson_files.each do |geojson_file_name|
+  line, direction, subline = geojson_file_name.split("_")
+
+  location_index = 0
+  geojson_file_hash = JSON.parse(file)
+
+  geojson_file_hash["features"].each do |feature|
+    params = {}
+    params[:line] = line
+    params[:direction] = direction
+    params[:subline] = subline
+    params[:location_index] = location_index
+
+    # puede que sea al reves
+    lon = feature["geometry"]["coordinates"][0]
+    lat = feature["geometry"]["coordinates"][1]
+
+    params[:location] = RGeo::Geographic.spherical_factory(srid: ENV['SRID']).point(lon, lat)
+    Detour.create!(params)
+
+    location_index += 1
+  end
+end
